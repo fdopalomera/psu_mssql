986ca38a3f7aa1db6a45d5d4ce2ce36d3a08969f
Fernando Palomera <fernando@MacBook-Pro-de-Fernando.local>
2020-06-26 10:22:14 -0400

    documentado ETL_PSU_Excel.py

--- ETL_PSU_Excel.py
+++ ETL_PSU_Excel.py
@@ -1,11 +1,23 @@
+""" Script para extraer, transformar y exportar a motor de sql bases psu en excel
+
+El siguiente script permite realizar un proceso de ETL a las bases PSU entregadas por casa central a la FEN, el cual
+es un archivo excel con las personas convocadas a matricularse y en lista de espera.
+"""
+
+
 from sqlalchemy import create_engine
 from class_PrepPsuExcel import PrepPsuExcel
 
-servername = 'localhost'
-database = 'psu'
-engine = create_engine('mssql://{}/{}?trusted_connection=yes&driver=ODBC+Driver+13+for+SQL+Server'.format(servername, database))
+
+# Ruta absoluta o relativa de la base deseada a procesoar
 file_route = 'bases_excel/p19_fen.xlsx'
 
+# Datos para generar la conexión a SQL Server
+servername = 'localhost'
+database = 'psu'
+engine = create_engine(f'mssql://{servername}/{database}?trusted_connection=yes&driver=ODBC+Driver+13+for+SQL+Server')
+
+# Diccionario con valores que se reconocen como equivalentes a datos nulos, según columna.
 nan_dict = {
     'PTJE_RANKING': 0, 
     'PTJE_NEM': 0,
@@ -25,9 +37,12 @@ nan_dict = {
     'CODIGO_COMUNA_D': 0,
 }
 
+
+# Se instancia la clase para realizar porceso de ETL
 df = PrepPsuExcel(file_route)
+# Se aplican las transformaciones/preproceso
 df.homolgate_columns_name()
-df.info()
 df.replace_nan_values(nan_dict)
+# Se exporta base a motor de sql
 df.to_sql(f'{file_route[12:19]}', engine, replace_if_exist=True)
 
